c8351a4cc42eac7f47fa8245c4ac2278cf9eddb8
afdalcitra <afdalcitra.edu@gmail.com>
2023-12-14 13:05:34 +0700

    Model and Middleware Init

---
+++ app/Models/Instrument.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Instrument extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'code', 'name', 'price', 'image', 'description',
+    ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'instrument_category');
+    }
+
+    public function reservation(): HasOne
+    {
+        return $this->hasOne(Reservation::class);
+    }
+}
---
+++ app/Models/Reservation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'instrument_id', 'tanggal_peminjaman', 'tanggal_dikembalikan', 'total_price', 'penalty'];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function setValidToValueAttribute($date)
+    {
+        $this->attributes['valid_to'] = strtotime($date);
+    }
+
+    public function instrument(): BelongsTo
+    {
+        return $this->belongsTo(Instrument::class, 'instrument_id');
+    }
+
+    public function save(array $options = [])
+    {
+        if (!$this->user_id) {
+            $this->user_id = Auth::id();
+        }
+        parent::save($options);
+    }
+}
